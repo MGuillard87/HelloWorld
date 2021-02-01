0c902bbac92151d037df4d15fab11fc22c497e09
Tsyppora <maty.la.gui@gmail.com>
2021-02-01 12:10:22 +0100

    afficher'hello' avec la commande Environnement.UserName le propriétaire de l'ordinateur

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            String nom_de_utilisateur = Environment.UserName;
+             Console.WriteLine("Hello " + nom_de_utilisateur + " !");
             //ajouter du code pour suspendre l’application afin que la fenêtre de console ne se ferme pas 
             Console.ReadLine();
         }
